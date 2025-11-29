02dc40bb7a109afc53336aaa9c907d2334618a35
jashwanth.j <jashwanth.j@servicenow.com>
2025-11-29 18:36:35 +0530

    TEST

--- frontend/src/views/authentication/Register.js
+++ frontend/src/views/authentication/Register.js
@@ -17,7 +17,7 @@ const userValidationSchema = yup.object({
   name: yup.string().min(2).max(25).required('Please enter your name'),
   email: yup.string('Enter your email').email('Enter a valid email').required('Email is required'),
   password: yup
-    .string('Enter your password')
+    .string('Enter your passwords')
     .min(6, 'Password should be of minimum 6 characters length')
     .required('Password is required'),
   confirm_password: yup
